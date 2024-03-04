a9f5683d3ef69b9f1e38dc1f939dd563ebeb24bd
Loris M <80474693+Loris-Moreau@users.noreply.github.com>
2024-03-04 11:00:42 +0100

    [add] item data

---
+++ Assets/Items/ItemData.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace Items
+{
+    [CreateAssetMenu(menuName = "Quest System/Item")]
+    public class ItemData : ScriptableObject
+    {
+        public Guid UID;
+        public string label;
+        public Sprite icon;
+    
+        private void OnValidate()
+        {
+#if UNITY_EDITOR
+            if (UID == Guid.Empty)
+            {
+                UID = Guid.NewGuid();
+                UnityEditor.EditorUtility.SetDirty(this);
+            }
+#endif
+        }
+
+        public bool Equals(ItemData data)
+        {
+            return data != null && UID == data.UID;
+        }
+    }
+}
