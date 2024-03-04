5620c53b6004cfdc2d04df9dc5d4e23193ce9bc7
ZoeChrvx <zchervaux@artfx.fr>
2024-03-04 10:38:41 +0100

    [add] Inventory + SO Quest item

---
+++ Assets/Inventory/ScriptObj QuestObject/SOQuestItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOQuestItem : ScriptableObject
+{
+    public Sprite sprite;
+    public GameObject gameObject;
+
+}
