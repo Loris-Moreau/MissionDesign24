a1ede1ce77ac5f6bb59066a0c1e950550838c3a7
Anthonin <akadi@artfx.fr>
2024-03-05 12:08:55 +0100

    [Finish] FPS

--- Assets/Character/PlayerController.cs
+++ Assets/Character/PlayerController.cs
@@ -1,5 +1,3 @@
-using System;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,13 +7,17 @@ namespace Character
     {
         [Header("Player Options")] 
         public float speed = 5;
+        public float mouseSensitivity = 2f;
+        private float rotationSpeed = 40;
+
         [Space]
-        
         [Header("Interaction")]
         public GameObject interactBox;
         public float interactBoxTime = 0.2f;
 
         private Vector2 direction;
+        private Vector2 movementInput;
+
 
         void Start() 
         {
@@ -25,6 +27,8 @@ namespace Character
 
         private void Update()
         {
+            RotateWithMouse();
+            MoveWithKeys();
             transform.position += speed * Time.deltaTime * new Vector3(direction.x, 0, direction.y);
         }
 
@@ -39,12 +43,32 @@ namespace Character
 
         public void Move(InputAction.CallbackContext context)
         {
-            direction = context.ReadValue<Vector2>();
+            movementInput = context.ReadValue<Vector2>();
         }
+        
+        void RotateWithMouse()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            transform.Rotate(Vector3.up * (mouseX * rotationSpeed * Time.deltaTime));
+        }
+        
+        void MoveWithKeys()
+        {
+            Quaternion cameraRotation = Camera.main.transform.rotation;
+
+            Vector3 moveDirection = new Vector3(movementInput.x, 0, movementInput.y);
+            moveDirection = cameraRotation * moveDirection;
+            
+            transform.Translate(moveDirection.normalized * (speed * Time.deltaTime), Space.World);
+        }
+
 
         private void DisableInteractBox()
         {
             interactBox.SetActive(false);
         }
+        
     }
 }
