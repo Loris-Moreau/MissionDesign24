1615fa6efc7bffbb7f0f530f231226ca8f3c6b6d
ZoeChrvx <zchervaux@artfx.fr>
2024-03-06 17:08:57 +0100

    [add] Folders Quest + [fix] QuestData name's variables

--- Assets/Quests/System/QuestData.cs
+++ Assets/Quests/System/QuestData.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quest System/Quest")]
 public class QuestData : ScriptableObject
 {
-    public string title, shortDescription;
+    public string title, objectif;
     [TextArea] public string description;
+    [TextArea] public List<string> subObjectif;
     [TextArea] public string thankYouMessage;
     
     public List<QuestItem> requirements = new List<QuestItem>();
