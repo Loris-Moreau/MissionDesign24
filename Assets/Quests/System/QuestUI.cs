51fd95ee24a31f40ebd9a666b58a22fbdf351482
ZoeChrvx <zchervaux@artfx.fr>
2024-03-07 13:04:23 +0100

    [add] debut UI

--- Assets/Quests/System/QuestUI.cs
+++ Assets/Quests/System/QuestUI.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class QuestUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static QuestUI instance;
+    public Dictionary<QuestData, GameObject> questPanel = new Dictionary<QuestData, GameObject>();
+    public Transform questPanelContainer;
+    public GameObject questPrefab;
+
+    public TextMeshPro textTitle;
+
+    public void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddQuest(QuestData quest)
     {
-        
+        textTitle.text = quest.title;
+        for (int i = 0; i < quest.subObjectif.Count; i++) 
+        {
+            Instantiate(questPrefab, questPanelContainer);
+            
+        }
+
+    }
+
+    public void RemoveQuest(QuestData quest)
+    {
+
     }
 }
